39f253bca27ddce2849d3feb0b72c8f6de6255b2
Atasi Kulkarni <75190043+atasikulkarni@users.noreply.github.com>
2021-06-30 10:55:15 +0530

    Add files via upload

--- backend/Models/tweetsModel.js
+++ backend/Models/tweetsModel.js
@@ -1,1 +1,13 @@
+const mongoose = require('mongoose');
+const TweetSchema = mongoose.Schema(
+    {
+        tweet: String,
+        userName: String,
+        userHandle: String,
+        isLiked: Boolean,
+        timeStamp: Number,
 
+    },
+    {collections:"Tweets"}
+);
+module.exports = mongoose.model("Tweets",TweetSchema);
